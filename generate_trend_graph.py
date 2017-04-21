ff0e8dc745760fb05f3f9a94d659379f010821cf
pangan <pangan@gmail.com>
2017-04-21 20:54:19 +0200

    added white space

--- generate_trend_graph.py
+++ generate_trend_graph.py
@@ -10,65 +10,64 @@
 """
 import ast
 import click
-import svgwrite as svgwrite
+import svgwrite
+
+
+def _bool_to_color(bool_value):
+    if bool_value:
+        return 'green'
+    return 'red'
+
+
+def _get_timestamp_and_status_color_from_data_string(data_string):
+    split_data = data_string.split()
+    time_stamp = int(split_data[0])
+    status_color = _bool_to_color(ast.literal_eval(split_data[1]))
+    return time_stamp, status_color
 
 
 def read_data_from_file(input_file, start_time, end_time):
-    # todo: remove filtered_data_dict
-    filtered_data_dict = []
     filtered_data_list = []
-    last_read_timestamp = None
-    last_status = False
+    last_read_timestamp = start_time
+    last_status = 'white'
+    # todo: write comment why white is used!
     width = 0
     try:
         for line in input_file:
-            #todo: make a function to return time_stamp and status!
-            split_data = line.split()
-            time_stamp = int(split_data[0])
+            time_stamp, status_color = _get_timestamp_and_status_color_from_data_string(line)
             if time_stamp > end_time:
                 break
-            status = ast.literal_eval(split_data[1])
-            if time_stamp >= start_time:
-                filtered_data_dict.append([time_stamp, status])
 
-                if last_read_timestamp:
-                    width += time_stamp - last_read_timestamp
-                    if last_status != status:
-                        #todo: remove print
-                        print width, last_status
+            if time_stamp >= start_time:
+                width += time_stamp - last_read_timestamp
+                if last_status != status_color:
                         filtered_data_list.append([width, last_status])
                         width = 0
 
                 last_read_timestamp = time_stamp
-                last_status = status
+                last_status = status_color
 
-        if last_read_timestamp:
-            width += end_time - last_read_timestamp
-            if width > 0:
-                filtered_data_list.append([width, last_status])
+        width += end_time - last_read_timestamp
+        if width > 0:
+            filtered_data_list.append([width, last_status])
 
     except Exception, e:
         print('Incorrect datafile format! {0}'.format(e))
         exit()
 
-    # todo: remove print
-    print filtered_data_dict
     return filtered_data_list
 
 
-def make_svg_from_data(list_of_data):
+def make_svg_from_data(list_of_data, total_width):
+    # todo: make output file from parameter!
     dwg = svgwrite.Drawing('test.svg')
-    dwg.viewbox(0, 0, 500, 50)
-    # todo: how to show if there r a lot of data ? and their width be very tiny ? maybe svg can handle automaticly?
-    # todo: width should be a percentage of 500 and also based on total
+    maximum_viewbox_width = 500.0
+    dwg.viewbox(0, 0, maximum_viewbox_width, 50)
     last_width = 0
-    for width, status in list_of_data:
-        fill_color = 'red'
-        if status:
-            fill_color = 'green'
-
-        dwg.add(dwg.rect((last_width, 0), (width, 50), fill=fill_color))
-        last_width += width
+    for width, status_color in list_of_data:
+        percentage_width = width * maximum_viewbox_width / total_width
+        dwg.add(dwg.rect((last_width, 0), (percentage_width, 50), fill=status_color))
+        last_width += percentage_width
 
     dwg.save()
 
@@ -81,7 +80,8 @@ def make_svg_from_data(list_of_data):
 def main(start_time, end_time, input_data_file, output_svg_file):
     list_of_filtered_events_width = read_data_from_file(input_data_file, start_time, end_time)
     print list_of_filtered_events_width
-    make_svg_from_data(list_of_filtered_events_width)
+    total_width = end_time - start_time
+    make_svg_from_data(list_of_filtered_events_width, total_width)
 
 if __name__ == '__main__':
     main()
